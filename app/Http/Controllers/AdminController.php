b5950d305823a6e61b72978789a30a2bbe2408a3
HusniFadhilah <husnifadhilah62@gmail.com>
2023-10-21 17:18:22 +0700

    Menambahkan admin controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\{EyeDisorder, Job, PastMedical, Patient, User};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,6 +11,13 @@ class AdminController extends Controller
     public function dashboard()
     {
         $authUser = Auth::user();
-        return view('roles.dashboard.index', compact('authUser'));
+        $countData = [
+            ['id' => 'users', 'title' => 'USER', 'bg' => 'success', 'count' => User::count(), 'icon' => 'fa fa-users', 'href' => route('user')],
+            ['id' => 'patients', 'title' => 'PASIEN', 'bg' => 'dark', 'count' => Patient::count(), 'icon' => 'icon-people', 'href' => route('patient')],
+            ['id' => 'jobs', 'title' => 'PEKERJAAN', 'bg' => 'warning', 'count' => Job::count(), 'icon' => 'icon-work', 'href' => route('job')],
+            ['id' => 'eyeDisorders', 'title' => 'KELUHAN MATA', 'bg' => 'danger', 'count' => EyeDisorder::count(), 'icon' => 'icon-eye', 'href' => route('eye-disorder')],
+            ['id' => 'pastMedicals', 'title' => 'RIWAYAT PENYAKIT', 'bg' => 'primary', 'count' => PastMedical::count(), 'icon' => 'ion-ios-medical', 'href' => route('past-medical')],
+        ];
+        return view('roles.dashboard.index', compact('authUser', 'countData'));
     }
 }
--- resources/views/roles/dashboard/index.blade.php
+++ resources/views/roles/dashboard/index.blade.php
@@ -3,5 +3,36 @@
 @section('title', 'Dashboard Admin')
 
 @section('content')
+<div class="container-fluid">
 
+    <!-- Page Heading -->
+    <div class="halo">
+        <hr>
+        <h4>Halo <strong>{{ $authUser->name }}</strong></h4>
+        Selamat Datang di <b>Dashboard Admin Web Deteksi Dini Katarak</b><br>
+        Role Anda adalah <b>{{ $authUser->role->name }}</b>
+        <hr>
+    </div>
+    <!-- Content Row -->
+    <div class="row">
+        @foreach ($countData as $data)
+        <a class="col-xl-3 col-md-6 mb-4 btn-nero text-decoration-none" href="{{ $data['href'] }}">
+            <div class="card bg-{{ $data['bg'] }} shadow h-100 py-2 home">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="h6 font-weight-bold text-light text-uppercase mb-1">{{ $data['title'] }}</div>
+                            <div class="h3 mb-0 font-weight-bold text-light">{{ $data['count'] }}</div>
+                        </div>
+                        <div class="col-auto homes">
+                            <i class="{{ $data['icon'] }} fa-4x text-light"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </a>
+        @endforeach
+    </div>
+    <!-- End of Row -->
+</div>
 @endsection
